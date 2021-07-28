9a195d7ae38f016c6c71cfcc3fe60ce71a6337e0
Michael Amiro <odindo.michael@gmail.com>
2021-07-28 10:46:19 +0300

    Initial Commit with one Component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+class Navbar extends Component {
+    render() { 
+        return (
+            <nav className="navbar navbar-dark bg-dark fixed-top">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Navbar</span>
+                </div>
+            </nav>
+        );
+    }
+}
+ 
+
+export default Navbar;
