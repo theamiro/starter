6cc60e6f0aeccfb2679d0511d9dff6c0f112252c
Michael Amiro <odindo.michael@gmail.com>
2021-08-09 15:43:12 +0300

    Added menu items

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,38 @@
-import React, { Component } from 'react'
+import React, {Component} from "react"
 class Navbar extends Component {
-    render() { 
-        return (
-            <nav className="navbar navbar-dark bg-dark fixed-top">
-                <div className="container-fluid">
-                    <span className="navbar-brand mb-0 h1">Navbar</span>
-                </div>
-            </nav>
-        );
-    }
+	render() {
+		return (
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+				<div className="container">
+					<a className="navbar-brand" href="https://">
+						Navbar
+					</a>
+					<ul className="navbar-nav ms-auto">
+						<li className="nav-item">
+							<a className="nav-link active" href="/">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="/">
+								About
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="/">
+								Documentation
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="/">
+								Help
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		)
+	}
 }
- 
 
-export default Navbar;
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import Navbar from './components/navbar'
+import React, {Component} from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "bootstrap/dist/css/bootstrap.css"
+import Navbar from "./components/navbar"
 
-ReactDOM.render(<Navbar />, document.getElementById("root"));
+ReactDOM.render(<Navbar />, document.getElementById("root"))
